a8093a985a06a6257a3efeb9b89a52cf12360279
fartbagxp <fartbagxp@gmail.com>
2022-02-18 20:09:10 -0500

    Retry all the curl calls (#10)

--- .github/backup.sh
+++ .github/backup.sh
@@ -6,22 +6,22 @@ set -eu
 target="${1}/data/raw"
 
 echo delegated-afrinic-extended-latest
-curl -o "${target}/delegated-afrinic-extended-latest.txt" ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-extended-latest
+curl --retry 5 -o "${target}/delegated-afrinic-extended-latest.txt" ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-extended-latest
 
 echo delegated-apnic-extended-latest
-curl -o "${target}/delegated-apnic-extended-latest.txt" ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-extended-latest
+curl --retry 5 -o "${target}/delegated-apnic-extended-latest.txt" ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-extended-latest
 
 echo delegated-arin-extended-latest
-curl -o "${target}/delegated-arin-extended-latest.txt" ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest
+curl --retry 5 -o "${target}/delegated-arin-extended-latest.txt" ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest
 
 echo delegated-lacnic-extended-latest
-curl -o "${target}/delegated-lacnic-extended-latest.txt" ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest
+curl --retry 5 -o "${target}/delegated-lacnic-extended-latest.txt" ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest
 
 echo delegated-ripencc-extended-latest
-curl -o "${target}/delegated-ripencc-extended-latest.txt" ftp://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-extended-latest
+curl --retry 5 -o "${target}/delegated-ripencc-extended-latest.txt" ftp://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-extended-latest
 
 echo ripe-ncc-allocated-list
-curl -o "${target}/ripe-ncc-alloclist.txt" ftp://ftp.ripe.net/pub/stats/ripencc/membership/alloclist.txt
+curl --retry 5 -o "${target}/ripe-ncc-alloclist.txt" ftp://ftp.ripe.net/pub/stats/ripencc/membership/alloclist.txt
 
 echo asn-listing
-curl -o "${target}/autnums.html" https://bgp.potaroo.net/cidr/autnums.html
+curl --retry 5 -o "${target}/autnums.html" https://bgp.potaroo.net/cidr/autnums.html
